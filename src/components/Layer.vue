9d9b6469b3e89dc24a576cc51d834354db3ef6e2
davide <davide.disalvo@thisispegasus.co.uk>
2019-11-05 15:02:55 +0000

    production

--- src/components/Layer.vue
+++ src/components/Layer.vue
@@ -119,9 +119,8 @@ font-family: 'Exo 2', sans-serif;
 
 #svgContainer {
   width:31vw;
-    @media only screen and (max-width: 720px) {
-      width: 50%;
-
+  @media only screen and (max-width: 720px) {
+    width: 50%;
   }
 }
 button {
